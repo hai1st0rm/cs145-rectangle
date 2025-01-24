aab27c6b597c8c59700f457680ac7c0130800a56
Maya Clarke <admin@bakermatic.xyz>
2025-01-24 14:42:15 -0800

    maybe got carried away. but it looks nice.

--- RectangleTest.java
+++ RectangleTest.java
@@ -13,21 +13,44 @@ public class RectangleTest
     public static void main(String[] args)
     {
         Scanner inScanner = new Scanner(System.in);
+        Scanner strScanner;
         int x1;
         int y1;
         int x2;
         int y2;
+        String point1;
+        String point2;
         System.out.println("Please enter some coordinates for a rectangle");
 
-        System.out.print("x1 > ");
-        x1 = inScanner.nextInt();
-        System.out.print("y1 > ");
-        y1 = inScanner.nextInt();
-        System.out.print("x2 > ");
-        x2 = inScanner.nextInt();
-        System.out.print("y2 > ");
-        y2 = inScanner.nextInt();
+        System.out.print("first point (x1,y1) > ");
+        point1 = inScanner.nextLine();
+        System.out.print("second point (x2,y2) > ");
+        point2 = inScanner.nextLine();
 
+        System.out.println("reading input...");
+
+        point1 = point1.replace("(", "");
+        point1 = point1.replace(")", "");
+        point1 = point1.replace(",", " ");
+
+        point2 = point2.replace("(", "");
+        point2 = point2.replace(")", "");
+        point2 = point2.replace(",", " ");
+
+        strScanner = new Scanner(point1);
+
+        x1 = strScanner.nextInt();
+        y1 = strScanner.nextInt();
+
+        strScanner.close();
+
+
+        strScanner = new Scanner(point2);
+
+        x2 = strScanner.nextInt();
+        y2 = strScanner.nextInt();
+
+        strScanner.close();
         // this is just testing stuff, remove it before submitting
         System.out.printf("coordinates entered: (%d,%d), (%d,%d)%n", x1, y1, x2, y2);
 
